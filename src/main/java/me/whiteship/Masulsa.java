13c107db4f03af839bfa6a1fd20b012c3946ccd4
miniLove <han419120@naver.com>
2022-10-15 23:44:22 +0900

    바이트 코드 조작 - 순서 정해주기

--- src/main/java/me/whiteship/Masulsa.java
+++ src/main/java/me/whiteship/Masulsa.java
@@ -1,7 +1,9 @@
 package me.whiteship;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,15 +12,18 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) {
-
+        ClassLoader classLoader = Masulsa.class.getClassLoader();
+        TypePool typePool = TypePool.Default.of(classLoader);
         try {
-            new ByteBuddy().redefine(Moja.class)
+            new ByteBuddy().redefine(typePool.describe("me.whiteship.Moja").resolve()
+                            , ClassFileLocator.ForClassLoader.of(classLoader))
                     .method(named("pullOut")).intercept(FixedValue.value("Rabbit"))
                     .make().saveIn(new File("C:/Users/PC/Git/java8/target/classes/"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        //설정이 점점 많아지면 다른데에서 모자 클래스가 먼저 읽힐 수 있는 가능성 있기 때문에 완벽하진 않다.
         System.out.println(new Moja().pullOut());
     }
 }
